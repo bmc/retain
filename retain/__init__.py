2bbf5e4d0cfa78206f77fb7b02f1388ba919e38a
Brian Clapper <bmc@clapper.org>
2019-04-12 20:24:47 -0400

    Updated to Py3

--- retain/__init__.py
+++ retain/__init__.py
@@ -22,33 +22,22 @@ Options:
 # Imports
 # ---------------------------------------------------------------------------
 
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-
-from builtins import (bytes, dict, int, list, object, range, str, ascii,
-                      chr, hex, input, next, oct, open, pow, round, super,
-                      filter, map, zip)
-from future.standard_library import install_aliases
-install_aliases()
-
 from docopt import docopt
-import string
 import sys
 import os
 import stat
 import shutil
+from typing import Sequence
 
 # Info about the module
-__version__   = '1.2.0'
+__version__   = '1.3.0'
 __author__    = 'Brian Clapper'
 __email__     = 'bmc@clapper.org'
 __url__       = 'http://github.com/bmc/retain'
-__copyright__ = '© 2003-2011 Brian M. Clapper'
+__copyright__ = '© 2003-2019 Brian M. Clapper'
 __license__   = 'BSD-style license'
 
 # Package stuff
-
-__all__     = ["retain"]
 
 # ---------------------------------------------------------------------------
 # Classes
@@ -61,32 +50,33 @@ class RetainException(Exception):
     def __str__(self):
         return str(self._value)
 
-    def get_value(self):
+    @property
+    def value(self):
         return self._value
 
-    value = property(get_value, doc="Get the string for the exception")
 
 class Verbose:
-    def __init__(self, verbose=False):
+    def __init__(self, verbose: bool = False):
         self._verbose = verbose
 
-    def println(self, msg):
+    def println(self, msg: str):
         if self._verbose:
             sys.stderr.write(msg + "\n")
 
-    def __call__(self, msg):
+    def __call__(self, msg: str):
         self.println(msg)
+
 
 class FileRetainer:
 
-    def __init__(self, argv):
+    def __init__(self, argv: Sequence[str]):
         self._parseParams(argv)
-        self._verbose = Verbose(self._verbose)
+        self._verbose = Verbose(self._be_verbose)
 
     def retain(self):
         verbose = self._verbose
 
-        verbose('Changing directory to "{0}"'.format(self._dir))
+        verbose(f'Changing directory to "{self._dir}"')
         try:
             os.chdir(self._dir)
 
@@ -100,30 +90,27 @@ class FileRetainer:
     # Private Methods
     # -----------------------------------------------------------------------
 
-    def _process_file(self, dir_file):
+    def _process_file(self, dir_file: str):
         verbose = self._verbose
         if dir_file in self._files:
-            verbose('Retaining "{0}"'.format(dir_file))
+            verbose(f'Retaining "{dir_file}"')
             return
         
-        verbose('Deleting "{0}"'.format(dir_file))
+        verbose(f'Deleting "{dir_file}"')
         if not self._no_exec:
             try:
                 mode = os.stat(dir_file)[stat.ST_MODE]
                 if stat.S_ISDIR(mode):
                     if not self._recursive:
-                        sys.stderr.write('Skipping directory "{0}"\n'.format(
-                            dir_file
-                        ))
+                        sys.stderr.write(f'Skipping directory "{dir_file}"')
                     else:
                         shutil.rmtree(dir_file)
                 else:
                     os.unlink(dir_file)
         
             except OSError as ex:
-                sys.stderr.write('Warning: Cannot delete "{0}": {1}\n'.format(
-                    dir_file, str(ex)
-                ))
+                print(f'Warning: Cannot delete "{dir_file}": {ex}',
+                      file=sys.stderr)
 
 
     def _parseParams(self, argv):
@@ -132,10 +119,10 @@ class FileRetainer:
         opts = docopt(__doc__, version=__version__)
         self._files = opts["<filename>"]
 
-        self._no_exec   = opts["--no-exec"]
-        self._verbose   = opts["--verbose"]
-        self._recursive = opts["--recursive"]
-        self._dir       = opts["--directory"] or "."
+        self._no_exec    = opts["--no-exec"]
+        self._be_verbose = opts["--verbose"]
+        self._recursive  = opts["--recursive"]
+        self._dir        = opts["--directory"] or "."
 
         if self._no_exec:
             self._verbose = True
