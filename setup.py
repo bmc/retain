b76c1708ee144b1370f6134e1025ed373982b9e5
Brian Clapper <bmc@clapper.org>
2023-05-24 15:30:04 -0400

    Added classifiers

--- setup.py
+++ setup.py
@@ -4,21 +4,27 @@
 # ---------------------------------------------------------------------------
 
 from setuptools import setup, find_packages
-import re
-import sys
-import os
 import retain
 
-setup (name="retain",
-       version=retain.__version__,
-       description="Delete all but the specified files/directories",
-       install_requires=[
-           'docopt >= 0.6.2',
-       ],
-       packages=find_packages(),
-       url=retain.__url__,
-       license=retain.__license__,
-       author=retain.__author__,
-       author_email=retain.__email__,
-       entry_points = {'console_scripts' : 'retain=retain:main'})
+setup (
+    name="retain",
+    version=retain.__version__,
+    description="Delete all but the specified files/directories",
+    install_requires=[
+        'click >= 8.0.0',
+    ],
+    packages=find_packages(),
+    url=retain.__url__,
+    license=retain.__license__,
+    author=retain.__author__,
+    author_email=retain.__email__,
+    entry_points = {'console_scripts' : 'retain=retain:main'},
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ]
+)
 
